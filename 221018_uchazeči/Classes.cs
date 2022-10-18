74d7947d7fa980ebf5853434bd86f0daa3df6f31
zelenkova.ma.2019 <zelenkova.ma.2019@UC313PC04>
2022-10-18 15:17:11 +0200

    vytvoření interace a validatorů

---
+++ 221018_uchazeči/Classes.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _221018_uchazeči
+{
+    class SurnameValidator :IStringValidator
+    {
+        public bool IsValid(string s)
+        {
+            if (s.Length != 0 && char.IsUpper(s[0]))
+                return true;
+            return false;
+        }
+    }
+
+    class AgeValidator:IIntValidator
+    {
+        public bool IsValid(string s, out int age)
+        {
+            if (int.TryParse(s, out age) == false)
+                return false;
+            if (age >= 18 || age < 70)
+                return true;
+            return false;
+        }
+    }
+
+    class WaiterAgeValidator : IIntValidator
+    {
+        public bool IsValid(string s, out int age)
+        {
+            if (int.TryParse(s, out age) == false)
+                return false;
+            if (age >= 15 || age < 50)
+                return true;
+            return false;
+        }
+    }
+
+    class LicenseValidator : ILicenceValidator
+    {
+        public bool HasLicense(string s)
+        {
+            if (s == "ano" || s == "Ano")
+                return true;
+            return false;
+        }
+    }
+
+    class ExperienceValidator : IExperienceValidator
+    {
+        public bool HasExperience(string s)
+        {
+            if (s == "ano" || s == "Ano")
+                return true;
+            return false;
+        }
+    }
+
+    class CriminalRecordValidator : ICriminalRecordValidator
+    {
+        public bool HasCleanCriminalRecord(string s)
+        {
+            if (s == "ano" || s == "Ano")
+                return true;
+            return false;
+        }
+    }
+}
---
+++ 221018_uchazeči/Interfaces.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _221018_uchazeči
+{
+    interface IIntValidator
+    {
+        bool IsValid(string s, out int i);
+    }
+
+    interface IStringValidator
+    {
+        bool IsValid(string s);
+    }
+
+    interface ILicenceValidator
+    {
+        bool HasLicense(string s);
+    }
+    interface IExperienceValidator
+    {
+        bool HasExperience(string s);
+    }
+
+    interface ICriminalRecordValidator
+    {
+        bool HasCleanCriminalRecord(string s);
+    }
+}
